cf6b8c836064b51c436132a3d73a937d2d9152ec
B. Hegmanns <hegmanns@gmx.de>
2022-10-03 13:23:42 +0200

    class names in english

--- junit5/junit5-practice/src/main/java/de/hegmanns/training/junit5/extension/training/junit5/practice/task16/ExchangeRateConverter.java
+++ junit5/junit5-practice/src/main/java/de/hegmanns/training/junit5/extension/training/junit5/practice/task16/ExchangeRateConverter.java
@@ -3,9 +3,9 @@ package de.hegmanns.training.junit5.extension.training.junit5.practice.task16;
 import java.math.BigDecimal;
 
 public class ExchangeRateConverter {
-    public static BigDecimal umrechnen(BigDecimal zero,
-                                       String ausgangswaehrungswaehrung,
-                                       String zielwaehrung) {
+    public static BigDecimal convert(BigDecimal zero,
+                                     String startCurrency,
+                                     String targetCurrency) {
         return BigDecimal.ZERO;
     }
 
